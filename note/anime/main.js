ca887ad6cb65c832002165e78bafac87ba8bb8f0
chabu-jp <chabu-jp@users.noreply.github.com>
2018-04-13 01:12:07 +0900

    use tabs

--- note/anime/main.js
+++ note/anime/main.js
@@ -1,72 +1,72 @@
 export default function (document, animes) {
-  // date desc
-  animes.sort((a, b) => {
-    return a[0] > b[0] ? -1 : 1;
-  });
+	// date desc
+	animes.sort((a, b) => {
+		return a[0] > b[0] ? -1 : 1;
+	});
 
-  let animes_by_year = new Map();
+	let animes_by_year = new Map();
 
-  // group by year
-  for (let anime of animes) {
-    // skip a watching or unfinished one
-    if (anime.length > 2) { continue; }
+	// group by year
+	for (let anime of animes) {
+		// skip a watching or unfinished one
+		if (anime.length > 2) { continue; }
 
-    let date = anime[0].split("-");
-    let year = Number.parseInt(date[0], 10);
-    //let month = Number.parseInt(date[1], 10);
+		let date = anime[0].split("-");
+		let year = Number.parseInt(date[0], 10);
+		//let month = Number.parseInt(date[1], 10);
 
-    if (animes_by_year.has(year)) {
-      animes_by_year.get(year).push(anime);
-    } else {
-      animes_by_year.set(year, [anime]);
-    }
-  }
+		if (animes_by_year.has(year)) {
+			animes_by_year.get(year).push(anime);
+		} else {
+			animes_by_year.set(year, [anime]);
+		}
+	}
 
-  let nodes = build_tree(document, animes_by_year);
+	let nodes = build_tree(document, animes_by_year);
 
-  nice_append(document, nodes);
+	nice_append(document, nodes);
 }
 
 function build_tree(document, animes_by_year) {
-  let sections = [];
+	let sections = [];
 
-  for (let [year, animes] of animes_by_year) {
-    let section = document.createElement("section");
-    let h2 = document.createElement("h2");
+	for (let [year, animes] of animes_by_year) {
+		let section = document.createElement("section");
+		let h2 = document.createElement("h2");
 
-    h2.textContent = `${year}年`;
-    section.appendChild(h2);
+		h2.textContent = `${year}年`;
+		section.appendChild(h2);
 
-    COMPOSE: {
-      let ul = document.createElement("ul");
+		COMPOSE: {
+			let ul = document.createElement("ul");
 
-      for (let anime of animes) {
-        let li = document.createElement("li");
-        li.textContent = anime[1];
-        ul.appendChild(li);
-      }
+			for (let anime of animes) {
+				let li = document.createElement("li");
+				li.textContent = anime[1];
+				ul.appendChild(li);
+			}
 
-      section.appendChild(ul);
-    }
+			section.appendChild(ul);
+		}
 
-    sections.push(section);
-  }
+		sections.push(section);
+	}
 
-  return sections;
+	return sections;
 }
 
 function nice_append(document, sections) {
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", sections => {
-      let node = document.querySelector("main");
-      for (let section of sections) {
-        node.appendChild(section);
-      }
-    });
-  } else {
-    let node = document.querySelector("main");
-    for (let section of sections) {
-      node.appendChild(section);
-    }
-  }
+	if (document.readyState === "loading") {
+		document.addEventListener("DOMContentLoaded", sections => {
+			let node = document.querySelector("main");
+			for (let section of sections) {
+				node.appendChild(section);
+			}
+		});
+	} else {
+		let node = document.querySelector("main");
+		for (let section of sections) {
+			node.appendChild(section);
+		}
+	}
 }
